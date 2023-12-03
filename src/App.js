71266940f6b0cd80df08312e2945670de6b17803
Alex Beltz <beltz.a@outlook.com>
2023-12-03 17:43:18 +0800

    Sidebar build and coores.content funct.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // Define the list of items for the sidebar
+  const sidebarItems = [
+    "SUMMARY",
+    "INVENTORY",
+    "AR",
+    "CASH",
+    "FOOD & BEVERAGE",
+    "HUMAN RESOURCES",
+    "EXPENDITURES",
+    "REVENUE",
+    "ROOMS",
+    "SALES & MARKETING",
+    "ASSET MAINTENANCE",
+    "SYSTEMS",
+    "FINANCIAL REPORTING",
+  ];
+
+  // State to keep track of the selected item
+  const [selectedItem, setSelectedItem] = useState(sidebarItems[0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        {sidebarItems.map((item, index) => (
+          <button
+            key={index}
+            className={`sidebar-item ${selectedItem === item ? "active" : ""}`}
+            onClick={() => setSelectedItem(item)}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+      <div className="content">
+        {/* Display content based on the selected item */}
+        <h1>{selectedItem}</h1>
+        {/* You can import and render the respective component based on the selected item */}
+        {/* Example: {selectedItem === "SUMMARY" && <SummaryComponent />} */}
+      </div>
     </div>
   );
 }
