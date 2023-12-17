fd963ac1ffefb0789f465086f48d446ac8be4ef5
Alex Beltz <beltz.a@outlook.com>
2023-12-17 18:39:14 +0800

    Rework data for dynamic aspect modals

--- src/AdminComponents/Inventory.jsx
+++ src/AdminComponents/Inventory.jsx
@@ -96,42 +96,81 @@ function Inventory() {
   // Data structure to store results for each category's aspects
   const aspectCriteria = {
     Access: {
-      Control: "Access to Inventory stores should be secured...",
-      Procedure: "Check if the inventory stores rooms...",
-      Points: "8",
+      Control: `Access to Inventory stores should be secured with lock and key, and restricted to assigned staff. The hotel will develop a key policy.`,
+      Procedure: `Check if the inventory stores rooms:
+      1. Secured by lock and key
+      2. Accesses restricted to assigned associates
+      3. Is key and access are documented on Log Book if using hard key?
+      4. Is a documented key policy in place and adhered to?
+      
+      In order to answer YES, all elements must be satisfied.`,
+      Points: `8`,
     },
     Requisition: {
-      Control: "Completed requisitions will be forwarded to cost control...",
-      Procedure: "Check if perpetual inventory records are kept...",
-      Points: "4",
+      Control: `Completed requisitions will be forwarded to cost control for entry into the perpetual inventory system (within one working day where cost/benefit supports it, otherwise at a minimum within two working days) and subsequent filing for permanent record. If a cost control function is not in place, the requisition will be entered into the system by the inventory clerks or in their absence by a member of the accounting department.
+
+      It is accepted that the involvement of the inventory clerk in updating the perpetual inventory system undermines the concept of segregation of duties. As a compensating control accounting will be required, on a surprise basis, to take a random sample of requisitions and trace them through to the updated inventory account.`,
+      Procedure: `Check if perpetual inventory records are kept:
+      1. Are they updated (within one working day, otherwise at a maximum within two working days) for both purchases received and requisitions made and signed as evidence of this
+      2. Where updates are not performed by a cost control function, does the accounting department carry out documented, random surprise spot checks on at least a quarterly basis, between purchase requests/ requisitions and recorded inventory?
+      
+      In order to answer YES, both elements must be satisfied.
+      
+      Note: If inventory is expensed directly , you may answer N/A.`,
+      Points: `4`,
     },
     System: {
-      Control: "Where inventory is held on the Balance Sheet...",
-      Procedure: "For all items maintained in inventory, check if...",
-      Points: "10",
+      Control: `Where inventory is held on the Balance Sheet and a perpetual inventory system is in place, all material variances between the perpetual inventory and the physical count should be investigated, resolved and signed-off by the Finance Lead.
+
+      Note:  Please answer 'N/A' where no inventory is held on the Balance Sheet  or no perpetual inventory system is in place`,
+      Procedure: `For all items maintained in inventory, check if :
+      1. Cyclical inventory counts must be performed for all stores, including sub-stores such as the F&B outlets. The counts will be structured in such a way that all items are counted at least once a quarter.
+      2. All inventory counts must be performed or supervised by an individual independent of the stores.
+      3. Stock counts must be blind, i.e. lines of stock are counted and the physical count reconciled to the perpetual inventory record.
+      4. All material variances between the perpetual inventory and the physical count will be investigated. The Finance Leader is required to review and sign document, investigate and resolved the imbalance stock.
+      5. If a material write-down of stock is anticipated, approvals consistent with the levels set for purchasing should be obtained.`,
+      Points: `10`,
     },
     Management: {
-      Control: "Requisitions are approved by the manager/ supervisor...",
-      Procedure:
-        "1. Interview storekeeper on the process when inventory is transferred...",
-      Points: "4",
+      Control: `Requisitions are approved by the manager/ supervisor before inventory is removed from  storerooms.  Requisitions are signed by both the storeroom and the outlet associate upon delivery of order.`,
+      Procedure: `1. Interview storekeeper on the process when inventory is transferred out of store, either by using inventory system or manual transfer.
+        2. Sample several requisition or internal transfer forms to verify if there are signatures of storeroom and outlet associates.`,
+      Points: `4`,
     },
     Beverage: {
-      Control:
-        "Par beverage inventories have been established in all stores...",
-      Procedure: "For each of the bars & restaurants:...",
-      Points: "6",
+      Control: `Par beverage inventories have been established in all stores and copies of these must be available at the F&B venue.
+        1. The food and beverage manager, in conjunction with the bar manager/captain will be tasked with establishing the par levels. These should be communicated to the cost controller or in his absence the accounting department.
+        2. Periodically, and at least quarterly, food and beverage management will spot-check the food and beverage stores to ensure par stock levels are maintained. This check will be evidenced.
+        3. Inter-outlet requisitioning must be limited to emergency situations only, and a procedure to control and record such transfers must be setup and documented.`,
+      Procedure: `For each of the bars & restaurants: 
+      1. Are par inventory levels established by the F&B Manager (or appropriate designate) and available at each venue
+      2. On at least a quarterly basis is a surprise spot check carried out and documented by the F&B Manager (or appropriate designate) to ensure that par liquor inventory levels are maintained; AND
+      3. Is this spot check documented? 
+      
+      In order to answer YES to this question, ALL elements must be satisfied.`,
+      Points: `6`,
     },
     Food: {
-      Control: "Par Food inventories have been established in all stores...",
-      Procedure: "For each of the food store:...",
-      Points: "6",
+      Control: `Par Food inventories have been established in all stores and copies of these must be available at the kitchen.
+      1. The Chef will be tasked with establishing the par levels. These should be communicated to the cost controller or in his absence the accounting department.
+      2. Periodically, and at least quarterly, Chef will spot-check the food and beverage stores to ensure par stock levels are maintained. This check will be evidenced.
+      3. Inter-kitchen requisitioning must be limited to emergency situations only, and a procedure to control and record such transfers must be setup and documented.`,
+      Procedure: `For each of the food store: 
+      1. Are par inventory levels established by the Chef(or appropriate designate) and available at Kitchen
+      2. On at least a quarterly basis is a surprise spot check carried out and documented by Chef (or appropriate designate) to ensure that par food inventory levels are maintained; AND
+      3. Is this spot check documented? 
+      
+      In order to answer YES to this question, ALL elements must be satisfied.`,
+      Points: `6`,
     },
     Duties: {
-      Control:
-        "Segregation of duties exists for purchasing and receiving functions...",
-      Procedure: "Check if these are applied on receiving:...",
-      Points: "4",
+      Control: `Segregation of duties exists for purchasing and receiving functions. If a purchasing and receiving department cannot be segregated due to limited staffing models, mitigating controls are implemented. Item received are properly controlled and checked by receiving staff.`,
+      Procedure: `Check if these are applied on receiving:
+      1. Is a sufficient separation between functions respected for the order, goods receipt and invoice verification (minimum 2 people are involved in these tasks ) ?
+      2. Are products delivered formally cross-checked against the order at the time of delivery (quantity, quality, weight, etc.)? 
+      
+      In order to answer YES, all elements must be satisfied.`,
+      Points: `4`,
     },
   };
 
